3c86dcaa988c3681faf58a693f71cf3b73a16daf
kiso <dotkiso@gmail.com>
2020-07-22 21:10:59 -0400

    manifest changes

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-      });
+    
 });
